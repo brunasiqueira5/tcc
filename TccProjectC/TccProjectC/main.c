5ffa6eed72399c40a012838b6d138a727cb9667e
brunasiqueira5 <101155418+brunasiqueira5@users.noreply.github.com>
2025-02-01 16:34:03 -0300

    main.c
    mudança da identificação da area

--- TccProjectC/TccProjectC/main.c
+++ TccProjectC/TccProjectC/main.c
@@ -7,21 +7,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Função para redimensionar e exibir imagens
+// FunÃ§Ã£o para redimensionar e exibir imagens
 void showAndWaitResized(const char* windowName, IplImage* img, int width, int height) {
     // Criar uma imagem de destino com o tamanho especificado
     IplImage* resizedImg = cvCreateImage(cvSize(width, height), img->depth, img->nChannels);
-    cvResize(img, resizedImg, CV_INTER_LINEAR); // Redimensionar usando interpolação linear
+    cvResize(img, resizedImg, CV_INTER_LINEAR); // Redimensionar usando interpolaÃ§Ã£o linear
 
     // Exibir a imagem redimensionada
     cvShowImage(windowName, resizedImg);
     cvWaitKey(0);
 
-    // Liberar a memória da imagem redimensionada
+    // Liberar a memÃ³ria da imagem redimensionada
     cvReleaseImage(&resizedImg);
 };
 
-// Função para processar contornos
+// FunÃ§Ã£o para processar contornos
 char getContours(IplImage* imgDil, IplImage* img) {
     CvMemStorage* storage = cvCreateMemStorage(0);
     CvSeq* contours = NULL;
@@ -39,27 +39,27 @@ char getContours(IplImage* imgDil, IplImage* img) {
         double area = cvContourArea(current, CV_WHOLE_SEQ, 0);
         //printf("Area do contorno: %.2f\n", area);
         
-        //Desenha os contornos da imagem quando a area for acima ou igual a 303212.00 (valor que peguei de uma area média das imagens) 
-        if (area > 1000) {
+        //Desenha os contornos da imagem quando a area for acima ou igual a 303212.00 (valor que peguei de uma area mÃ©dia das imagens) 
+        if (area > 100) {
             float perimetro = cvArcLength(current, CV_WHOLE_SEQ, 0);
 
-            // Criar um novo armazenamento para a aproximação do polígono
+            // Criar um novo armazenamento para a aproximaÃ§Ã£o do polÃ­gono
             CvMemStorage* storage_poly = cvCreateMemStorage(0);
 
-            // Criar uma sequência para armazenar os contornos aproximados
+            // Criar uma sequÃªncia para armazenar os contornos aproximados
             CvSeq* approx = cvApproxPoly(
                 current,             // Contorno de entrada
                 sizeof(CvContour),   // Tamanho do contorno
-                storage_poly,        // Armazenamento da memória
-                CV_POLY_APPROX_DP,   // Tipo de aproximação (Douglas-Peucker)
-                0.0005 * perimetro,   // Precisão da aproximação (2% do perímetro)
+                storage_poly,        // Armazenamento da memÃ³ria
+                CV_POLY_APPROX_DP,   // Tipo de aproximaÃ§Ã£o (Douglas-Peucker)
+                0.0005 * perimetro,   // PrecisÃ£o da aproximaÃ§Ã£o (2% do perÃ­metro)
                 1                    // Fechar o contorno (1 = fechado, 0 = aberto)
             );
 
             // Desenhar os contornos aproximados na imagem original
             cvDrawContours(img, approx, CV_RGB(0, 255, 0), CV_RGB(0, 0, 255), 0, 2, 8, cvPoint(0,0));
 
-            // Liberar memória do armazenamento usado
+            // Liberar memÃ³ria do armazenamento usado
             cvReleaseMemStorage(&storage_poly);
         }
         
@@ -67,7 +67,7 @@ char getContours(IplImage* imgDil, IplImage* img) {
 
     return(img, num_contours);
 
-    // Liberar memória
+    // Liberar memÃ³ria
     cvReleaseMemStorage(&storage);
 }
 
@@ -89,24 +89,24 @@ int main() {
         IplImage* img = cvLoadImage(imgPath, CV_LOAD_IMAGE_COLOR);
         if (img == NULL) {
             //printf("Nao foi possivel carregar a imagem: %s\n", imgPath);
-            break; // Encerrar o loop ao atingir o final da sequência
+            break; // Encerrar o loop ao atingir o final da sequÃªncia
         }
 
         // Converter para escala de cinza
         IplImage* imgGray = cvCreateImage(cvGetSize(img), IPL_DEPTH_8U, 1);
         cvCvtColor(img, imgGray, CV_BGR2GRAY);
 
-        // Aplicar limiarização para binarizar a imagem
+        // Aplicar limiarizaÃ§Ã£o para binarizar a imagem
         IplImage* imgThreshold = cvCreateImage(cvGetSize(imgGray), IPL_DEPTH_8U, 1);
         cvAdaptiveThreshold(imgGray, imgThreshold, 255, CV_ADAPTIVE_THRESH_MEAN_C, CV_THRESH_BINARY, 15, 1);
 
-        // Aplicar dilatação para melhorar a detecção de contornos
+        // Aplicar dilataÃ§Ã£o para melhorar a detecÃ§Ã£o de contornos
         IplImage* imgDil = cvCreateImage(cvGetSize(imgThreshold), IPL_DEPTH_8U, 1);
         cvDilate(imgThreshold, imgDil, NULL, 1);
 
         getContours(imgDil, img);
 
-        // Exibir imagens redimensionadas para verificação
+        // Exibir imagens redimensionadas para verificaÃ§Ã£o
         //showAndWaitResized(grayWindow, imgGray, imgGray->width / 2, imgGray->height / 2); //  Reduzido pela metade
         //showAndWaitResized(dilateWindow, imgDil, imgDil->width / 2, imgDil->height / 2); //  Reduzido pela metade
         showAndWaitResized(dilateWindow, img, img->width / 2, img->height / 2);
@@ -114,7 +114,7 @@ int main() {
         // Processar os contornos
         
 
-        // Liberar memória para as imagens processadas
+        // Liberar memÃ³ria para as imagens processadas
         cvReleaseImage(&img);
         cvReleaseImage(&imgGray);
         cvReleaseImage(&imgThreshold);
